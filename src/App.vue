4ab596d2c268670388f3a206466477ff9dc97db0
Iban <iban.moreno.martin@gmail.com>
2020-02-27 17:42:10 +0100

    basic conf

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>{{$d(new Date(),'short')}}</h1>
+    <div class="locale-changer">
+      <select v-model="$i18n.locale">
+        <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">{{ lang }}</option>
+      </select>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return { langs: ['es', 'eu'] }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
